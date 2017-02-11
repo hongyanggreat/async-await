1312a63d6baa14df6a6f5c086b513b9dde89f1ff
Admin <vanpho02@gmail.com>
2017-02-11 09:25:53 +0700

    cai dat moi truong, let + arrow function

---
+++ index.js
@@ -0,0 +1,15 @@
+let mang = [4, 3, 7, 5, 1];
+
+// mang.forEach(e => console.log(e));
+
+var mang2 = mang.map(e => e * 2);
+
+console.log(mang2);
+
+// function add(a, b){
+//   return a + b;
+// }
+
+let add = (a, b) => a + b;
+
+console.log(add(4, 5));
