bbc3477dea339a2016dcdd321f4a7b0197a86f3e
Admin <vanpho02@gmail.com>
2017-02-11 09:56:59 +0700

    on tap function javascript

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let mang = [4, 3, 7, 5, 1];
 
 // mang.forEach(e => console.log(e));
+var nhan2 = e => e * 2;
+var mang2 = mang.map(nhan2);
 
-var mang2 = mang.map(e => e * 2);
-
-console.log(mang2);
+// console.log(mang2);
 
 // function add(a, b){
 //   return a + b;
@@ -12,4 +12,15 @@ console.log(mang2);
 
 let add = (a, b) => a + b;
 
-console.log(add(4, 5));
+// console.log(add(4, 5));
+
+let getFunction = (num) => {
+  if(num >= 0) return () => console.log('So duong');
+  return () => console.log('So am')
+}
+
+// getFunction(1)();
+
+let a = () => {}
+
+console.log(a());
